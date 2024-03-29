e668514b72378899859bc0a8984bd3172a9e154f
foryou929 <zxcvwert3457@gmail.com>
2024-03-29 16:36:16 +0900

    feat: fix button width by size

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,9 +7,9 @@ interface ButtonComponentProps {
 
 const Button: React.FC<ButtonComponentProps> = ({ className, children, type, size }) => {
     return (
-        <button className={`w-[240px] ${type == "primary" ? "bg-primary" : "bg-secondary"} text-black rounded-full ${size == "xl" ? "py-[24px]" : size == "xs" ? "py-[12px]" : "py-[16px]"} xl:text-xl font-bold ${className}`}>
+        <button className={`${size == "xl" ? "w-[320px]" : size == "xs" ? "w-[160px]" : "w-[240px]"} ${type == "primary" ? "bg - primary" : "bg - secondary"} text-black rounded-full ${size == "xl" ? "py - [24px]" : size == "xs" ? "py - [12px]" : "py - [16px]"} xl:text-xl font-bold ${className}`}>
             {children}
-        </button>
+        </button >
     )
 }
 
